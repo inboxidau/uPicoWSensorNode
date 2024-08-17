3d99cd8b0b8f8c284855767a8d66f37fd5ef7d05
inboxidau <neil.walker@inbox.id.au>
2024-08-18 02:36:41 +0930

    Update SAMPLE Atmospheric main.py

--- SAMPLE Atmospheric main.py
+++ SAMPLE Atmospheric main.py
@@ -8,17 +8,19 @@ log = URollingAppenderLog("AtmosphericSensorNode.log",
                           print_messages=True,
                           log_level=AtmosphericSensorNode.STATIC_NODE_LOG_LEVEL)  # noqa: E501
 
-myNode = AtmosphericSensorNode(log=log, config_path="config.json")
+myNode = AtmosphericSensorNode(log=log, config_path="myconfig.json")
 
 while True:
     try:
-        print("CONSOLE: starting myNode")
+        print("CONSOLE: starting AtmosphericSensorNode myNode")
         log.log_message("main.py starting myNode", LogLevel.INFO)
         myNode.main()
+
     except Exception as e:
-        print(f"CONSOLE: exception in myNode ({e})")
+        print(f"CONSOLE: exception in AtmosphericSensorNode myNode ({e})")
         log.log_message(f"main.py {str(e)}", LogLevel.ERROR)
 
     print(f"CONSOLE: retry in {myNode.STATIC_NODE_RESTART_DELAY}.")
+
     log.log_message(f"main.py retry in {myNode.STATIC_NODE_RESTART_DELAY} seconds.", LogLevel.INFO)  # noqa: E501
     time.sleep(myNode.STATIC_NODE_RESTART_DELAY)
