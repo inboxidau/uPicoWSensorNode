83320be1ce81ace21afae864a279a390bc0657f2
inboxidau <neil.walker@inbox.id.au>
2024-08-18 02:29:01 +0930

    Update SAMPLE Atmospheric main.py

--- SAMPLE Atmospheric main.py
+++ SAMPLE Atmospheric main.py
@@ -1,9 +1,12 @@
 from lib.inboxidau.rolling_appender_log import URollingAppenderLog, LogLevel
-from lib.inboxidau.pico_w_sensor_node import UPicoWSensorNode
 import time
 from AtmosphericSensorNode import AtmosphericSensorNode
 
-log = URollingAppenderLog("AtmosphericSensorNode.log", max_file_size_bytes=1024, max_backups=10, print_messages=True, log_level=AtmosphericSensorNode.STATIC_NODE_LOG_LEVEL)
+log = URollingAppenderLog("AtmosphericSensorNode.log",
+                          max_file_size_bytes=1024,
+                          max_backups=10,
+                          print_messages=True,
+                          log_level=AtmosphericSensorNode.STATIC_NODE_LOG_LEVEL)  # noqa: E501
 
 myNode = AtmosphericSensorNode(log=log, config_path="config.json")
 
@@ -15,7 +18,7 @@ while True:
     except Exception as e:
         print(f"CONSOLE: exception in myNode ({e})")
         log.log_message(f"main.py {str(e)}", LogLevel.ERROR)
-        
+
     print(f"CONSOLE: retry in {myNode.STATIC_NODE_RESTART_DELAY}.")
-    log.log_message(f"main.py retry in {myNode.STATIC_NODE_RESTART_DELAY} seconds.", LogLevel.INFO)
+    log.log_message(f"main.py retry in {myNode.STATIC_NODE_RESTART_DELAY} seconds.", LogLevel.INFO)  # noqa: E501
     time.sleep(myNode.STATIC_NODE_RESTART_DELAY)
